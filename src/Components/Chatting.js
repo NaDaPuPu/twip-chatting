81ade16dd76bb1b0da67debca7c35a6f4c571df1
NaDaPuPu <jun19998@gmail.com>
2021-06-02 17:59:13 +0900

    목규 스킨 Alpha 0.1v

---
+++ src/Components/Chatting.js
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from "react";
+import "./Chatting.css";
+
+const Chatting = () => {
+  const [chatText, setChatText] = useState("");
+  const [chatList, setChatList] = useState([]);
+
+  const nextId = useRef(1);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setChatText(value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const chat = {
+      id: nextId.current,
+      message: chatText,
+    };
+    setChatList([...chatList, chat]);
+    setChatText("");
+    nextId.current += 1;
+  };
+
+  return (
+    <div className="chattingContainer">
+      <div id="log">
+        {chatList.map((chat) => (
+          <div id={chat.id}>
+            <span className="meta">
+              <span className="name">onejun0319</span>
+              <span className="colon">:</span>
+            </span>
+            <span className="message">{chat.message}</span>
+          </div>
+        ))}
+      </div>
+      <div className="textArea" onSubmit={onSubmit}>
+        <form>
+          <input type="text" value={chatText} onChange={onChange}></input>
+          <input type="submit"></input>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Chatting;
